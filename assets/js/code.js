0bdfd1863617fd9481710836d026effdb1e3ac6c
lisa1Q84 <lisafeigen@web.de>
2021-01-27 19:13:21 +0000

    added functionality to carousel

--- assets/js/code.js
+++ assets/js/code.js
@@ -18,9 +18,7 @@ btn1No.addEventListener('click', function(){
 
 
 
-
-
-/* FUNCTION TO SHOW CONTENT BASED ON WHAT USER ANSWERS TO QUESTION 2 */ 
+//FUNCTION TO SHOW CONTENT BASED ON WHAT USER ANSWERS TO QUESTION 2 
 
 var btn2Yes = document.getElementById('button-fix-yes');
 var btn2No  = document.getElementById('button-fix-no');
@@ -64,34 +62,12 @@ btn2No.addEventListener('click', function(){
 
 })
 
-/* FUNCTION TO SHOW CONTENT BASED ON WHAT USER ANSWERS TO QUESTION 3 */ 
+// FUNCTION TO SHOW CONTENT BASED ON WHAT USER ANSWERS TO QUESTION 3  
 
 
-var btnYes3 = document.getElementById('option3yes');
-var btnNo3  = document.getElementById('option3no');
-var optYes3 = document.getElementById('self-care');
-var optNo3  = document.getElementById('option1-no');
 
-btnYes3.addEventListener('click', function(){
-  optYes3.className = ''; 
-  optNo3.className = 'hidden';
-  $("#badDayQuestion").hide();
-  $(".container.q1").hide()
-  $("#gif-container").hide()
-  $(".image-container").hide();
-});
 
-btnNo3.addEventListener('click', function(){
-  optNo3.className = '';
-  optYes3.className = 'hidden';
-  $("#badDayQuestion").hide();
-  $(".container.q1").hide()
-  $("#gif-container").hide()
-  $(".image-container").show();
-
-});
-
-/* FUNCTION TO PRESS NEXT BUTTON TO GO TO GRATITUDE LIST */ 
+// FUNCTION TO PRESS NEXT BUTTON TO GO TO GRATITUDE LIST 
 
 
 
@@ -113,17 +89,126 @@ var getworries = localStorage.getItem('userWorries');
 document.getElementById("put-worries").textContent= getworries
 
 
+
+
+/* SELF CARE CAROUSEL */
+
+const track = document.querySelector('.carousel__track');
+const slides =Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// put slides next to each other
+
+const setSlidePosition = (slide, index) => {
+slide.style.left= slideWidth * index + 'px' }
+
+slides.forEach(setSlidePosition);
+
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform =' translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+
+}
+
+const hideShowArrows = (slides, targetIndex, prevButton, nextButton) => {
+      if (targetIndex === 0) {
+        prevButton.classList.add('hidden')
+        nextButton.classList.remove('hidden')
+    } else if (targetIndex === slides.lenght -1 ){
+        prevButton.classList.remove('hidden')
+        nextButton.classList.add('hidden')
+    } else {
+        prevButton.classList.remove('hidden')
+        nextButton.classList.remove('hidden')
+    }}
+
+//move slide to the left when button 'left' is clicked
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot= currentDot.previousElementSibling;
+    const prevIndex= slides.findIndex(slide => slide === prevSlide)
+ 
+   
+    moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot);
+    hideShowArrows (slides, prevIndex, prevButton, nextButton)
+
+
+})
+
+//move slide to the right when button 'right' is clicked
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot= currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide)
+
+    
+    updateDots(currentDot, nextDot);
+    moveToSlide(track, currentSlide, nextSlide);
+    hideShowArrows (slides, nextIndex, prevButton, nextButton)
+})
+
+//when I click on dots move to that slide
+
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+    
+    if(!targetDot) return
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+    
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, targetIndex, prevButton, nextButton)
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* GRATITUDE LIST */ 
 
 
 //selectors
-const gratitudeInput = document.querySelector('.gratitude_input');
-const gratitudeButton = document.querySelector('.gratitude_button');
-const gratitudeList = document.querySelector('.gratitude_list');
-const filterOption = document.querySelector('.filter_gratitude');
+    const gratitudeInput = document.querySelector('.gratitude_input');
+    const gratitudeButton = document.querySelector('.gratitude_button');
+    const gratitudeList = document.querySelector('.gratitude_list');
+    const filterOption = document.querySelector('.filter_gratitude');
 //event listeners
-gratitudeButton.addEventListener("click", addgratitude)
-gratitudeList.addEventListener("click", deleteCheck)
+    gratitudeButton.addEventListener("click", addgratitude)
+    gratitudeList.addEventListener("click", deleteCheck)
 //functions
 
 function addgratitude(event) {
